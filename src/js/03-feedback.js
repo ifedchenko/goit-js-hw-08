dac06bff8a19a4f5553040fec47eb93563060338
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-05-04 22:51:35 +0300

    finish task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textArea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+onReloadPage();
+
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onReloadPage() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    input.value = savedData.email;
+    textArea.value = savedData.message;
+  }
+}
