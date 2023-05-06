d4204ca88a942bdb159cdc0e1ae13d89ca2f7308
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-05-06 16:18:42 +0300

    Bugfix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,33 +4,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 let getPlayerCurrentTime = localStorage.getItem('videoplayer-current-time');
+// console.log(getPlayerCurrentTime);
 
 player.on('timeupdate', throttle(onGetCurrentTime, 1000));
 
-function onGetCurrentTime() {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+function onGetCurrentTime(evt) {
+  // console.log(evt);
+  // console.log(evt.seconds);
+  const eventTime = evt.seconds;
+  localStorage.setItem('videoplayer-current-time', eventTime);
 }
 
 function onReloadPage() {
-  player
-    .setCurrentTime(getPlayerCurrentTime)
-    .then(function () {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-
-        default:
-          break;
-      }
-    });
+  getPlayerCurrentTime === undefined || getPlayerCurrentTime === null
+    ? localStorage.setItem('videoplayer-current-time', 0)
+    : player.setCurrentTime(getPlayerCurrentTime);
 }
 
 onReloadPage();
